2d765ced025ac488594d2657629f5bcdcf7d6500
motanesku <60298950+motanesku@users.noreply.github.com>
2026-04-08 10:31:18 +0300

    Add company aliases for stock symbols

---
+++ app/data/company_aliases.py
@@ -0,0 +1,95 @@
+# File: app/data/company_aliases.py
+
+COMPANY_ALIASES = {
+    "NVDA": [
+        "nvidia",
+        "nvidia corp",
+        "nvidia corporation",
+    ],
+    "AMD": [
+        "advanced micro devices",
+        "amd",
+    ],
+    "AAPL": [
+        "apple",
+        "apple inc",
+    ],
+    "MSFT": [
+        "microsoft",
+        "microsoft corp",
+        "microsoft corporation",
+    ],
+    "GOOGL": [
+        "alphabet",
+        "google",
+        "alphabet inc",
+        "google parent alphabet",
+    ],
+    "AMZN": [
+        "amazon",
+        "amazon.com",
+        "amazon com",
+    ],
+    "META": [
+        "meta",
+        "meta platforms",
+        "facebook",
+    ],
+    "PLTR": [
+        "palantir",
+        "palantir technologies",
+    ],
+    "LMT": [
+        "lockheed martin",
+        "lockheed",
+    ],
+    "VRTX": [
+        "vertex pharmaceuticals",
+        "vertex pharma",
+        "vertex",
+    ],
+    "XOM": [
+        "exxon",
+        "exxon mobil",
+        "exxonmobil",
+    ],
+    "CVX": [
+        "chevron",
+        "chevron corp",
+        "chevron corporation",
+    ],
+    "CCJ": [
+        "cameco",
+    ],
+    "SMCI": [
+        "super micro",
+        "super micro computer",
+        "supermicro",
+    ],
+    "ANET": [
+        "arista",
+        "arista networks",
+    ],
+    "PANW": [
+        "palo alto",
+        "palo alto networks",
+    ],
+    "CRWD": [
+        "crowdstrike",
+    ],
+    "TSM": [
+        "tsmc",
+        "taiwan semiconductor",
+        "taiwan semiconductor manufacturing",
+    ],
+    "INTC": [
+        "intel",
+        "intel corp",
+        "intel corporation",
+    ],
+    "SAFE": [
+        "safetech",
+        "safetech innovations",
+        "safe",
+    ],
+}
